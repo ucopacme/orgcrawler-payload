e380e0a291955f230968f6c0111d5138ba4e3163
David Rivera <david.rivera@ucop.edu>
2019-09-04 09:01:30 -0700

    add local only cf

--- orgcrawler/untested_payload/cloudformation.py
+++ orgcrawler/untested_payload/cloudformation.py
@@ -6,3 +6,10 @@ def cf_list_stack_sets(region, account):
     response = client.list_stack_sets()
     response.pop('ResponseMetadata')
     return response
+
+
+def cf_list_stacks(region, account):
+    client = boto3.client('cloudformation', region_name=region, **account.credentials)
+    response = client.list_stacks()
+    response.pop('ResponseMetadata')
+    return response
