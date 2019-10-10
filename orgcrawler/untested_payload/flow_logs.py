307432e69bb9e74853e259f4dea53efaa8776541
David Rivera <david.rivera@ucop.edu>
2019-10-10 15:18:12 -0700

    created flog log payload function for future usage of IS3

--- orgcrawler/untested_payload/flow_logs.py
+++ orgcrawler/untested_payload/flow_logs.py
@@ -3,7 +3,9 @@ import boto3
 
 def create_flow_log(region, account):
     client = boto3.client('ec2', region_name=region, **account.credentials)
-    vpc_list = [vpc[VpcId] for vpc in client.describe_vpcs()]
-    response = client.create_flow_logs(ResourceType='VPC',TrafficType='ALL',LogDestinationType='s3',LogDestination='arn:aws:s3:::qradar-cloudtrailbucket-1tltlpn8gpsa2-logbucket-1gahs58bwbwlv' , ResourceIds=vpc_list )
+    vpc_list = [vpc['VpcId'] for vpc in client.describe_vpcs()["Vpcs"]]
+    print(vpc_list)
+    response = client.create_flow_logs(ResourceType='VPC',TrafficType='ALL',LogDestinationType='s3',LogDestination='arn:aws:s3:::ait-flow-logs/*' ,ResourceIds=vpc_list)
+    print(response)
     response.pop('ResponseMetadata')
     return response
