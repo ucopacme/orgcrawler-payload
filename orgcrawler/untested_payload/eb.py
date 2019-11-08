f58f5128918075b7ec52a85eb0c9854defc2851c
santhosh <santhosh.katakam@ucop.edu>
2019-11-08 13:47:17 -0800

    update function

--- orgcrawler/untested_payload/eb.py
+++ orgcrawler/untested_payload/eb.py
@@ -1,11 +1,11 @@
 import boto3
 
 
-def paginate(client, method, **kwargs):
-    paginator = client.get_paginator(method.__name__)
-    for page in paginator.paginate(**kwargs).result_key_iters():
-        for result in page:
-            yield result
+#def paginate(client, method, **kwargs):
+#    paginator = client.get_paginator(method.__name__)
+#    for page in paginator.paginate(**kwargs).result_key_iters():
+#        for result in page:
+#            yield result
 
 
 def list_eb_environments(region, account):
@@ -13,7 +13,7 @@ def list_eb_environments(region, account):
     orgcrawler -r OrganizationAccountAccessRole orgcrawler.untested_payload.eb.list_eb_environments
     '''
     client = boto3.client('elasticbeanstalk', region_name=region, **account.credentials)
-    env_generator = paginate(client, client.describe_environments)
+    envs_ = client.describe_environments(client, account, region)
     env_names = [env['EnvironmentName'] for env in env_generator]
     return dict(Keys=env_names)
 
