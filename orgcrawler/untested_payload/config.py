e101e120ac1e9e1d2398e7b22f74369fb4e60875
Ashley Gould <agould@ucop.edu>
2020-03-30 14:47:20 -0700

    adding payloads to manage default security groups. work in progress
    new payload funcs so far:
    def get_rule_by_regex(region, account, regex):
    def paginate(client, method, **kwargs):
    def get_non_compliant_resources(region, account, rule_name):
    def list_rules_for_non_compliant_sg(region, account, rule_name):

--- orgcrawler/untested_payload/config.py
+++ orgcrawler/untested_payload/config.py
@@ -1,3 +1,4 @@
+import re
 import boto3
 
 
@@ -27,14 +28,26 @@ def describe_rules(region, account):
       orgcrawler -r OrganizationAccountAccessRole --regions us-west-2 orgcrawler.untested_payload.config.describe_rules | jq -r '.[] | .Account, (.Regions[] | ."us-west-2".ConfigRules[].ConfigRuleName), ""' | tee config_rules_in_accounts.us-west-2
     '''
     client = boto3.client('config', region_name=region, **account.credentials)
-    response = client.describe_config_rules()
-    rules = response['ConfigRules']
-    while 'NextToken' in response:
-        response = client.describe_config_rules(NextToken=response['NextToken'])
-        rules += response['ConfigRules']
-    return dict(ConfigRules=rules)
+    rule_paganator = paginate(client, client.describe_config_rules)
+    return dict(ConfigRules=[rule for rule in rule_paganator])
 
 
+def get_rule_by_regex(region, account, regex):
+    '''
+    return list of config rule names matching regex
+
+      orgcrawler -r awsauth/OrgAdmin --regions us-west-2 orgcrawler.untested_payload.config.get_rule_by_regex securityhub
+
+    '''
+    config_rules = describe_rules(region, account)['ConfigRules']
+    rule_re = re.compile(regex)
+    matching_rules = [r['ConfigRuleName'] for r in config_rules if rule_re.match(r['ConfigRuleName'])]
+    return dict(MatchingRules=matching_rules)
+
+
+        
+
+ 
 def count_rules(region, account, pattern=None):
     '''
     usage example:
@@ -57,17 +70,45 @@ def count_rules(region, account, pattern=None):
     return dict(Count=len(matching_rules))
 
 
+def paginate(client, method, **kwargs):
+    paginator = client.get_paginator(method.__name__)
+    for page in paginator.paginate(**kwargs).result_key_iters():
+        for result in page:
+            yield result
+
+
 def get_compliance_details(region, account, rule_name):
     '''
-      orgcrawler -r awsauth/OrgAdmin --regions us-west-2 orgcrawler.untested_payload.config.get_compliance_details COMPLIANCE_RULESET_LATEST_INSTALLED
-      #| jq -r '.[] | .Account, .Regions[].Output.Count'
+    rule_name can be a regex
 
-      botocore.errorfactory.NoSuchConfigRuleException: An error occurred (NoSuchConfigRuleException)
+      orgcrawler -r awsauth/OrgAdmin --regions us-west-2 orgcrawler.untested_payload.config.get_compliance_details COMPLIANCE_RULESET_LATEST_INSTALLED
+
     '''
     client = boto3.client('config', region_name=region, **account.credentials)
-    try:
+    matching_rules = get_rule_by_regex(region, account, rule_name)['MatchingRules']
+    compliance_details = []
+    for rule_name in matching_rules:
         response = client.get_compliance_details_by_config_rule(ConfigRuleName=rule_name)
         response.pop('ResponseMetadata')
-        return response
-    except client.exceptions.NoSuchConfigRuleException as ex:
-        return {}
+        compliance_details.append(response)
+    return dict(ComplianceDetails=compliance_details)
+
+
+def get_non_compliant_resources(region, account, rule_name):
+    '''
+    rule_name can be a regex
+
+      orgcrawler -r awsauth/OrgAdmin --regions us-west-2 orgcrawler.untested_payload.config.get_non_compliant_resources securityhub-vpc-default
+    '''
+    client = boto3.client('config', region_name=region, **account.credentials)
+    compliance_details = get_compliance_details(region, account, rule_name)['ComplianceDetails']
+    non_compliant_resources = []
+    for rule_evaluation in compliance_details:
+        for result in rule_evaluation['EvaluationResults']:
+            if result['ComplianceType'] == 'NON_COMPLIANT':
+                non_compliant_resources.append(result['EvaluationResultIdentifier']['EvaluationResultQualifier'])
+    return dict(NonCompliantResources=non_compliant_resources)
+
+
+
+
--- orgcrawler/untested_payload/ec2.py
+++ orgcrawler/untested_payload/ec2.py
@@ -1,4 +1,5 @@
 import boto3
+from .config import get_non_compliant_resources
 
 
 def list_ec2_instances(region, account):
@@ -19,3 +20,26 @@ def list_vpn_gateways(region, account):
     response = client.describe_vpn_gateways()
     response.pop('ResponseMetadata')
     return response
+
+
+def list_rules_for_non_compliant_sg(region, account, rule_name):
+    '''
+    Query config service for non compliant security groups per given config rule.
+    
+    rule_name can be a regex
+
+      orgcrawler -r awsauth/OrgAdmin --regions us-west-2 orgcrawler.untested_payload.ec2.list_rules_for_non_compliant_sg securityhub-vpc-default
+    '''
+    non_compliant_resources = get_non_compliant_resources(region, account, rule_name)['NonCompliantResources']
+    non_compliant_sg = [r['ResourceId'] for r in non_compliant_resources if r['ResourceType'] == 'AWS::EC2::SecurityGroup']
+    client = boto3.client('ec2', region_name=region, **account.credentials)
+    ec2 = boto3.resource('ec2', region_name=region, **account.credentials)
+    sg_rules = []
+    for sg_id in non_compliant_sg:
+        sg = ec2.SecurityGroup(sg_id)
+        sg_rules.append(dict(
+            group_id=sg_id,
+            ingress_rules=sg.ip_permissions,
+            egress_rules=sg.ip_permissions_egress,
+        ))
+    return dict(NonCompliantSecurityGroupRules=sg_rules)
